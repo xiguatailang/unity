ba3d27e0015e940beb29ed4c23052265cc96ae1f
liuwei <504890176@qq.com>
2017-08-19 20:36:21 +0800

    create ku

---
+++ Assets/cs/wall/wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour {
+	public GameObject housePrefeb;
+
+	// Use this for initialization
+	void Start () {
+//		GameObject myCube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+//		myCube.transform.position = new Vector3 (1,2,3);
+//		myCube.transform.localEulerAngles = new Vector3 (2,2,2);
+//		myCube.transform.localScale = new Vector3 (3,3,3);
+
+		for (int i = 0; i < 10; i++) {
+			GameObject myHouse = GameObject.Instantiate (housePrefeb);
+			myHouse.transform.position = new Vector3 (1+i*15,1,1);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
